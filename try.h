63d7d3b1408991e523d58dc0a70c773d027d828f
Remo Dentato <rdentato@gmail.com>
2021-01-17 16:04:13 +0100

    Update try.h
    throw() is now a real function (rather than a macro).
    Added try_abort() to avoid having CATCH_HANDLER defined as a macro in the user space.

--- try.h
+++ try.h
@@ -18,7 +18,7 @@
   try {
      ... code ...
      if (something_failed) throw(execption_num)  // must be > 0 
-     some_other_func(); // you can throw exceptions from other functions too 
+     some_other_func(); // you can trhow exceptions from other functions too 
      ... code ...
   }  
   catch(OUTOFMEM) {
@@ -33,8 +33,10 @@
                  
 ]]] */
 
-#ifndef TRY_H__
-#define TRY_H__
+#ifndef TRY_VERSION
+#define TRY_VERSION 0x0001000C
+#define TRY_VERSION_STR "try 0.1.0rc"
+
 #include <stdio.h>
 #include <setjmp.h>
 #include <assert.h>
@@ -65,42 +67,53 @@ extern char const *try_emptystring;
 
 #ifdef TRY_MAIN
   try_t try__;
+
+  #define CATCH_HANDLER 0
+  int try_abort() { assert(CATCH_HANDLER);}
+
+  int try_throw(int x, int y, char *fname, int line) 
+  {
+    if (try_jmp_list == NULL) try_abort(); 
+    if (x > 0) {\
+      try_jmp_list->fn  = fname; \
+      try_jmp_list->ln  = line;
+      try_jmp_list->id  = y;
+      longjmp(try_jmp_list->jb, x);
+    } 
+    return x;
+  }
+
 #endif
 
-#define CATCH_HANDLER 0
 
 #define try_INIT     {.pv = try_jmp_list, .nn = 0, .fn = try_emptystring, .ln = 0, .id = 0}
 
 #define try          for ( try_jb_t try_jb = try_INIT; \
                           (try_jb.nn-- <= 0) && (try_jmp_list = &try_jb); \
                            try_jmp_list = try_jb.pv, try_jb.nn = (try_jb.ex == 0? 2 : try_jb.nn)) \
-                            if (try_jb.nn < -1) assert(CATCH_HANDLER); \
+                            if (try_jb.nn < -1) try_abort(); \
                        else if (((try_jb.ex = setjmp(try_jb.jb)) == 0)) 
 
 #define catch(x)       else if ((try_jb.ex == (x)) && (try_jmp_list=try_jb.pv, try_jb.nn=2)) 
 
 #define catchall       else for ( try_jmp_list=try_jb.pv; try_jb.nn < 0; try_jb.nn=2) 
 
-#define throw2(x,y)  do { int ex_ = x; \
-                       if (ex_ > 0) { \
-                         if (!try_jmp_list) assert(CATCH_HANDLER); \
-                         try_jmp_list->fn  = __FILE__; \
-                         try_jmp_list->ln  = __LINE__;\
-                         try_jmp_list->id  = (y);\
-                         longjmp(try_jmp_list->jb, ex_); \
-                       } \
-                     } while (0)
-
-#define tryexp(x) x
+int try_throw(int x, int y, char *fname, int line);
+int try_abort();
+  
+#define try_exp(x) x
 #define try_0(x,...)   x
 #define try_1(x,y,...) y
-#define throw(...)   throw2(tryexp(try_0(__VA_ARGS__,1)), \
-                            tryexp(try_1(__VA_ARGS__,0,0)))
+#define throw(...)  try_throw(try_exp(try_0(__VA_ARGS__,1)), \
+                              try_exp(try_1(__VA_ARGS__,0,0)),\
+                               __FILE__, __LINE__)
 
-#define rethrow()    throw2(try_jb.ex, try_jb.id)
+#define rethrow()    try_throw(try_jb.ex, try_jb.id, __FILE__, __LINE__)
 #define thrown()     try_jb.ex
 #define thrownid()   try_jb.id
 #define thrownfile() try_jb.fn
 #define thrownline() try_jb.ln
+
+// #define leave(e)    for(int e_=!!(e); e_; e_=-1) if (e_<0) goto leave; else
 
 #endif
