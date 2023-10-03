f7e8beef93437344f978e1af722fff83bace0c84
rdentato <rdentato@gmail.com>
2023-10-03 18:51:47 +0200

    Renamed variables.

---
+++ try.c
@@ -0,0 +1,11 @@
+/*
+**  (C) by Remo Dentato (rdentato@gmail.com)
+** 
+** This software is distributed under the terms of the MIT license:
+**  https://opensource.org/licenses/MIT
+**  
+*/
+
+#include "try.h"
+
+try_t catch = 0;
