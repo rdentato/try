d33bda7298621e773f15d57cf5f014ae2bf2233a
rdentato <rdentato@gmail.com>
2023-10-07 19:19:05 +0200

    Testing custom abort handler.

---
+++ test/test00.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "trytest.h"
+
+
+#include "try.h"
+
+#define tryabort(e) my_handler(e)
+
+void my_handler(exception_t *e)
+{
+  fprintf(stderr,"PASS: Unhandled exception %d @ %s:%d\n",e->exception_num, e->file_name, e->line_num);
+}
+
+try_t catch = 0;
+
+int main(int argc,char *argv[])
+{
+  try {
+    tstout("PASS: Throwing an unhandled exception will abort.");
+    throw(EX_OUTOFMEM);
+    tstout("FAIL: SHOULD NOT BE HERE.");
+  }
+  catch(EX_NODB) {
+    tstout("FAIL: GOT EXCEPTION %d.", thrown());
+  }
+  exit(1);
+}
